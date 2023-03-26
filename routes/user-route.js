f23dc39bfddf78165c30ac60ba8bb7756ecb302f
Alper ARI <alper.hamza.ari@gmail.com>
2023-03-26 15:37:37 +0300

    Improve user-route

--- routes/user-route.js
+++ routes/user-route.js
@@ -4,10 +4,28 @@ const { requireAuth } = require('../middlewares/auth');
 
 const router = Router();
 
+// Endpoints--------------------------------------------------------------
+
+// Get my user
 router.get('/', requireAuth, async (req, res) => {
   const { user } = req;
 
   return res.json({ user });
+});
+
+// Get other user
+// TODO: only authenticated users can get other users
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = User.findById(id);
+
+    return res.json({ user });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ error });
+  }
 });
 
 module.exports = router;
